c37eeeb20c625c513ea5bd916a3861599e92d7bc
xedrftgyhu7 <gs1lestacat@hotmail.com>
2024-05-06 00:30:44 +0700

    update aptos project

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-const randomNumber = (min, max) =>
-  Math.floor(Math.random() * (max - min + 1) + min);
+function findTheDifference(s, t) {
+  let result = 0;
+  for (const char of s) {
+    result ^= char.charCodeAt(0);
+  }
+  for (const char of t) {
+    result ^= char.charCodeAt(0);
+  }
+  return String.fromCharCode(result);
+}
