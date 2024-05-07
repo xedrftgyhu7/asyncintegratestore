86d56143287fc21ffd2f3f89db25fe19b616bd2b
xedrftgyhu7 <gs1lestacat@hotmail.com>
2024-05-07 13:36:58 +0700

    start project

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-function minDepth(root) {
-  if (!root) return 0;
-  if (!root.left && !root.right) return 1;
-  let min = Infinity;
-  if (root.left) min = Math.min(min, minDepth(root.left));
-  if (root.right) min = Math.min(min, minDepth(root.right));
-  return min + 1;
-}
+const gcdRecursive = (a, b) => {
+  if (b === 0) {
+    return a;
+  }
+  return gcdRecursive(b, a % b);
+};
