8007aa926745d8dcdba27ef5ab40ae8d8cb6374b
xedrftgyhu7 <gs1lestacat@hotmail.com>
2024-05-08 20:15:32 +0700

    add more features

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const gcdRecursive = (a, b) => {
-  if (b === 0) {
-    return a;
-  }
-  return gcdRecursive(b, a % b);
-};
+function lowestCommonAncestor(root, p, q) {
+  if (!root || root === p || root === q) return root;
+  const left = lowestCommonAncestor(root.left, p, q);
+  const right = lowestCommonAncestor(root.right, p, q);
+  if (left && right) return root;
+  return left ? left : right;
+}
