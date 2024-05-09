e2e55556e745079bcef534859fab5cdccb37d497
xedrftgyhu7 <gs1lestacat@hotmail.com>
2024-05-10 05:36:58 +0700

    update aptos project

--- index.js
+++ index.js
@@ -1,7 +1,1 @@
-function lowestCommonAncestor(root, p, q) {
-  if (!root || root === p || root === q) return root;
-  const left = lowestCommonAncestor(root.left, p, q);
-  const right = lowestCommonAncestor(root.right, p, q);
-  if (left && right) return root;
-  return left ? left : right;
-}
+const minN = (arr, n = 1) => [...arr].sort((a, b) => a - b).slice(0, n);
